ba30f2129b6df0fa9b115552a1a06550d93538b3
chaitak-gorai <chaitakgorai@gmail.com>
2022-04-24 00:08:48 +0530

    📝 Home page

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
-
-const Product = () => {
-  return <div>Product</div>;
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+const Product = ({ product: { image, name, slug, price, details } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            alt='headphones'
+            className='product-image'
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-name'>${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
